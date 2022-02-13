b278e202059ad7cf6f9a56144ca2b70affc89500
Muhammad Afifudin <afif.x.udin@gmail.com>
2022-02-13 13:03:42 +0700

    chore: setup apollo client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
+import { ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
+import { useApollo } from '../lib/apollo';
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const apolloClient = useApollo(pageProps);
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
