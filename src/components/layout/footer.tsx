5fa2c7e8a4dd9e5e6512cc83171c218d3f1340ce
Muhammad Afifudin <afif.x.udin@gmail.com>
2022-02-13 21:03:11 +0700

    feat: update layout

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,16 +1,31 @@
+import { FaGithub } from 'react-icons/fa';
+
 export default function Footer() {
   return (
-    <footer className="bg-black px-3 py-4 md:px-6">
-      <div className="max-w-4xl mx-auto">
-        Made by{' '}
-        <a
-          href="https://www.linkedin.com/in/m-afifudin/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-yellow-400 hover:underline"
-        >
-          Afifudin
-        </a>
+    <footer className="bg-black px-3 py-3 md:px-6">
+      <div className="max-w-4xl mx-auto flex justify-between items-center">
+        <div>
+          Made by{' '}
+          <a
+            href="https://www.linkedin.com/in/m-afifudin/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-yellow-400 hover:underline"
+          >
+            Afifudin
+          </a>
+        </div>
+        <div>
+          <a
+            href="https://github.com/afiiif/starwars"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-yellow-400 hover:underline block px-2 py-1"
+            aria-label="Github link"
+          >
+            <FaGithub size={24} />
+          </a>
+        </div>
       </div>
     </footer>
   );
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -18,7 +18,7 @@ export default function Layout({ children }: Props) {
 
       <Nav />
 
-      <main className="max-w-4xl mx-auto px-3 py-8 md:px-6 min-h-[calc(100vh_-_180px)] md:min-h-[calc(100vh_-_156px)]">
+      <main className="max-w-4xl mx-auto px-3 py-8 md:px-6 min-h-[calc(100vh_-_182px)] md:min-h-[calc(100vh_-_160px)]">
         {children}
       </main>
 
